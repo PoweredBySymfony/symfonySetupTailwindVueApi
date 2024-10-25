c52b45bb420a151563155f7282f6c452ed249795
Trouche Xavier <xavier.trouche@etu.umontpellier.fr>
2024-10-25 19:11:43 +0200

    mise en place fixtures entité

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\UX\Vue\VueBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
